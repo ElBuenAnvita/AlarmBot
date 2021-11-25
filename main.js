6a071090ec2c08605e1a1bf95a4b3d06bdd6202e
ElBuenAnvita <josegabrielanva@hotmail.com>
2021-11-25 13:56:20 -0500

    1.2

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ setInterval(function () {
             });
         }
     })
-}, 500);
+}, 25000);
 
 client.on('message', message => {
     let args = message.content.substring(prefix.length).split(" ");
